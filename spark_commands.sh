a096212dc4e897789d7e75d6d36864024ae17098
Felipe Oliveira <fe_g_oliveira@outlook.com>
2024-06-12 21:16:12 -0300

    Salvando o Token do Git no meu scrit

--- spark_commands.sh
+++ spark_commands.sh
@@ -1,3 +1,6 @@
+#Token Github
+ghp_dHmphG1s9fJ1vsiiXESXVu727EL8Kq32HBJ1
+
 # Iniciar um nó mestre do Spark
 /opt/spark/sbin/start-master.sh
 
@@ -11,3 +14,5 @@ run-example SparkPi
 
 # Contador de palavras de um determinado arquivo
 run-example JavaWordCount kern.log
+
+
