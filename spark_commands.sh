32fc441ce269bb61fdeab7cfd376437f35c88700
Felipe Oliveira <fe_g_oliveira@outlook.com>
2024-06-16 19:07:23 -0300

    Operações em RDD - Parte 1

--- spark_commands.sh
+++ spark_commands.sh
@@ -15,4 +15,25 @@ run-example SparkPi
 # Contador de palavras de um determinado arquivo
 run-example JavaWordCount kern.log
 
+# Executando alguns comandos no Pyspark
 
+#Aplicando filtro na minha lista numeros - Transformação para filtrar
+filtro = numeros.filter(lambda filtro: filtro > 2)
+#Ação para mostrar o resultado
+filtro.collect()
+
+# Multiplicando cada número da minha lista por 2 - Ação para transformar
+mapa = numeros.map(lambda mapa: mapa * 2)
+#Ação para mostrar o resultado
+mapa.collect()
+
+#Registrar dados em comum de dois conjuntos diferentes
+#Unificando os dados
+uniao = numeros.union(numeros2)
+#Chamando o método
+interseccao = numeros.intersection(numeros2)
+#Exibindo
+interseccao.collet()
+
+#Exibindo dados de um conjunto que não aparecem no outro
+subtrai = numeros.subtract(numeros2)
