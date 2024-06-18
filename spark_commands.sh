53acf2985ea22b614b5909d8e000ccb7cbfdb170
Felipe Oliveira <fe_g_oliveira@outlook.com>
2024-06-17 21:24:31 -0300

    RDD - Parte 2

--- spark_commands.sh
+++ spark_commands.sh
@@ -37,3 +37,29 @@ interseccao.collet()
 
 #Exibindo dados de um conjunto que não aparecem no outro
 subtrai = numeros.subtract(numeros2)
+
+#Criando variável com chave-valor
+compras = sc.parallelize([(1,200),(2,300),(3,120),(4,250),(5,78)])
+
+#Método RDD para armazenar as minhas chaves em uma variável e depois mostrá-la
+chaves = compras.keys()
+chaves.collect()
+
+#Armazenando os valores
+valores = compras.values()
+valores.collect()
+
+#Somando +1 em cada valor
+soma = compras.mapValues(lambda soma: soma + 1)
+soma.collect()
+
+#Criando a variável debitos para conter as keys que tiveram debito
+debitos = sc.parallelize([(1,20),(2,300)])
+
+#Assimilando qual o valor da compra e quanto foi debitado
+resultado = compras.join(debitos)
+resultado.colletc()
+
+#Trazendo quais as compras que não tiveram debitos
+semdebito = compras.subtractByKey(debitos)
+semdebito.collect()
