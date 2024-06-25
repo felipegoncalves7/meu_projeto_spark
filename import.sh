483b4ebbd4842c9cd2fbe0c0506ae9185cf5cf4f
Felipe Oliveira <fe_g_oliveira@outlook.com>
2024-06-25 20:37:02 -0300

    Imports utilizados durante o aprendizado

---
+++ import.sh
@@ -0,0 +1,7 @@
+#Registro de todos os imports utilizados
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum
+from pyspark.sql.functions import expr
+from pyspark.sql.types import *
+from pyspark.sql import functions as Func
+from pyspark.sql.functions import *
